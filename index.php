12f6f847a049c099f2c80693662f4fd1daa407fc
Viktor Jarnheimer <jarnheimer@gmail.com>
2019-01-13 13:52:02 +0100

    New courses

--- index.php
+++ index.php
@@ -10,15 +10,20 @@ $titel = "Ann-Sofie Jarnheimer - Leg. Sjukgymnast, Leg. Psykoterapeut";
 
         <div class="columnRight">
             <div class="head">Erfarenhet och kompetens</div>
-            <div class="text">Min dubbla kompetens och m&aring;ng&aring;riga erfarenhet g&ouml;r det m&ouml;jligt att
+            <div class="text">
+                <p>Min dubbla kompetens och m&aring;ng&aring;riga erfarenhet g&ouml;r det m&ouml;jligt att
                 behandla dina symtom fr&aring;n b&aring;de ett psykiskt och ett kroppsligt perspektiv. Mina kunskaper
                 inom b&aring;da omr&aring;dena ger en uppskattad f&ouml;rst&aring;else f&ouml;r helheten och mycket goda
-                behandlingsresultat och utv&auml;rderingar.<br>
-                Sedan &ouml;ver 25 &aring;r driver jag en privat praktik som &auml;r kopplad till högkostnadsskydd.
-                Jag f&ouml;rel&auml;ser och har konsultuppdrag &ouml;ver hela Sverige.<br>
-                <br>
-                <b>Aktuellt</b><br>
-                Stresshantering på Kreta (sep 2018)
+                    behandlingsresultat och utv&auml;rderingar.</p>
+                <p>Sedan &ouml;ver 30 &aring;r driver jag en privat praktik som &auml;r kopplad till högkostnadsskydd.
+                    Jag f&ouml;rel&auml;ser och har konsultuppdrag &ouml;ver hela Sverige.</p>
+                <p><b>Aktuellt</b></p>
+                <ul>
+                    <li>Ny besöks- och postadress</li>
+                    <li>Stresshantering, Kollbergagården 30 - 31 mars 2019</li>
+                    <li><a href="http://www.lendasoasen.se/kurser/stresshantering-pa-kreta/">Stresshantering - på kreta,
+                            19 – 26 september 2019</a></li>
+                </ul>
             </div>
         </div>
 
--- kurser.php
+++ kurser.php
@@ -11,34 +11,17 @@ $titel = "Ann-Sofie Jarnheimer - Kurser";
         <div class="columnRight">
             <div class="head">Kurser</div>
             <div class="text">
-                <b>Aktuellt</b><br>
-                <a href="http://www.lendasoasen.se/kurser/varva-ner-om-ledarskap-och-stresshantering/">Varva ner! En kurs i ledarskap och stresshantering</a> (maj 2017)
 
 
-                <p>Varje termin anordnas kurser inom stresshantering genom min uppdragsgivare Kalmar Arbetsgivarring.
-                    Kurserna l&ouml;per dagtid under en period p&aring; 12 veckor. De f&ouml;rsta sex veckorna tr&auml;ffas
-                    vi 6 timmar (2 x 3h) och de resterande sex veckorna 3 timmar per vecka. <br>
-                    F&ouml;r exakt schema, programpunkter, utv&auml;rderingar och kostnad kontakta mig eller Ulla
-                    Larsson p&aring; Arbetsgivarring, tel 0480-28115. </p>
-
-                <p>Under kursen undervisar jag/samtalar vi kring:<br>
+                <p><b>Aktuellt</b></p>
                 <ul>
-                    <li>Hur uppt&auml;cker vi signaler p&aring; stress?</li>
-                    <li>Kunskaper kring stress</li>
-                    <li>&Aring;ngest/oro vad fyller det f&ouml;r funktion?</li>
-                    <li>Gr&auml;nss&auml;ttning</li>
-                    <li>Tankef&auml;llor</li>
-                    <li>Hur vi bekr&auml;ftar andra och oss sj&auml;lva.</li>
-                    <li>Kommunikation &ndash; konflikthantering</li>
-                    <li>Andningen</li>
-                    <li>Olika avslappningsmetoder</li>
-                    <li>Medveten n&auml;rvaro</li>
-                    <li>Syskonpositionens roll</li>
-                    <li>Hur n&auml;rmar vi oss varandra. Att k&auml;nna tillit eller inte.</li>
-                    <li>Hur vi kan f&ouml;rb&auml;ttra v&aring;r s&ouml;mn?</li>
-                    <li>V&aring;ra livsv&auml;rden.</li>
-                    <li>Hur g&aring;r vi vidare?</li>
+                    <li>Stresshantering, Kollbergagården 30 - 31 mars 2019</li>
+                    <li><a href="http://www.lendasoasen.se/kurser/stresshantering-pa-kreta/">Stresshantering - på kreta,
+                            19 – 26 september 2019</a></li>
                 </ul>
+
+
+
             </div>
         </div>
   
